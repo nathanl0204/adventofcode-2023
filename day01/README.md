fcc384899fe0b9cf810520e6d92f8896a3122e9a
nathanl0204 <nathan.lienard@telecomnancy.eu>
2023-12-01 17:26:29 +0100

    Ajout de la solution au premier challenge

---
+++ day01/README.md
@@ -0,0 +1,2 @@
+Langage : Python
+Commande(s) à exécuter : python3 challenge01.md
---
+++ day01/challenge01.py
@@ -0,0 +1,23 @@
+chiffres = ["0","1","2","3","4","5","6","7","8","9"]
+
+def somme_calibration_values(nom_fichier):
+    fichier = open(nom_fichier)
+    L = fichier.readlines()
+    L = [mot.rstrip() for mot in L]
+    somme = 0
+    for mot in L:
+        for i in range(len(mot)):
+            if mot[i] in chiffres:
+                premier_chiffre = mot[i]
+                break
+        for i in range(1,len(mot)+1):
+            if mot[-i] in chiffres:
+                dernier_chiffre = mot[-i]
+                break
+        calibration_value = premier_chiffre + dernier_chiffre
+        somme += int(calibration_value)
+    fichier.close()
+    return somme
+
+if __name__ == '__main__':
+    print("Réponse : " + str(somme_calibration_values('input.txt')))
